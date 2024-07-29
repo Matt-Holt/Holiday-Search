1b7976cb687efa532a58fe81d41a8e8af0ea35f0
Matt-Holt <77441858+Matt-Holt@users.noreply.github.com>
2024-07-29 20:48:57 +0100

    Created first test
    Tests to see if first the flight returned is flight 2, but it obviously failed since it is not implemented yet

--- Holiday Search/HolidaySearch.cs
+++ Holiday Search/HolidaySearch.cs
@@ -8,6 +8,31 @@ namespace Holiday_Search
 {
     class HolidaySearch
     {
+        /*
+         * #### Customer #1
+         * ##### Input
+         * Departing from: Manchester Airport (MAN)
+         * Traveling to: Malaga Airport (AGP)
+         * Departure Date: 2023/07/01
+         * Duration: 7 nights
+         * */
+        public string DepartingFrom;
+        public string TravelingTo;
+        public DateTime DepartureDate;
+        public int Duration;
+        public SearchResult Result;
 
+        public HolidaySearch(string DepartingFrom, string TravelingTo, DateTime DepartureDate, int Duration)
+        {
+            this.DepartingFrom = DepartingFrom;
+            this.TravelingTo = TravelingTo;
+            this.DepartureDate = DepartureDate;
+            this.Duration = Duration;
+        }
+
+        public void Search()
+        {
+
+        }
     }
 }
--- Holiday Search/SearchTests.cs
+++ Holiday Search/SearchTests.cs
@@ -6,10 +6,26 @@ namespace Holiday_Search
     [TestClass]
     public class SearchTests
     {
+        /*
+         * #### Customer #1
+         * ##### Input
+         * Departing from: Manchester Airport (MAN)
+         * Traveling to: Malaga Airport (AGP)
+         * Departure Date: 2023/07/01
+         * Duration: 7 nights
+         * 
+         * ##### Expected result
+         * Flight 2 and Hotel 9
+         */
         [TestMethod]
-        public void TestMethod1()
+        public void Test1_ToAGPFromMan7Nights()
         {
+            HolidaySearch search = new HolidaySearch("MAN", "AGP", new DateTime(2023, 7, 1), 7);
+            search.Search();
 
+            //Check flight
+            Flight flight = search.Result.Flight;
+            Assert.IsTrue(flight.Id == 2);
         }
     }
 }
